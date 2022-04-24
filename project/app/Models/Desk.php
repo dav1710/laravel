9bcc04a6c9a0016411e388c84c5dc9d4acca3c4e
David Muradyan <davokazarov99@gmail.com>
2022-04-24 20:51:26 +0400

    new api

---
+++ project/app/Models/Desk.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Desk extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['token'];
+
+    /**
+ * Prepare a date for array / JSON serialization.
+ *
+ * @param  \DateTimeInterface  $date
+ * @return string
+ */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s
+        ');
+    }
+}
